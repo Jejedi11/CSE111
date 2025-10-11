9386c9f8ff9de3fc8e521660cfb85cd3cb02711c
James Michelson <jejedi110@gmail.com>
2025-10-11 10:44:43 -0700

    loop

--- final_project/mp3_player.py
+++ final_project/mp3_player.py
@@ -8,6 +8,14 @@ def play_song(song):
     player.play()
     time.sleep(10)
 
+def list_folder(folder):
+    index = 1
+    song_list = os.listdir(folder)
+    for i in song_list:
+        print(f"{index}: {i}")
+        index += 1
+    return song_list
+
 def load_playlist(playlist_file):
     pass
 
@@ -15,14 +23,16 @@ def make_playlist():
     pass
 
 def main():
-    index = 1
-    print("Select a song to play.")
-    song_list = os.listdir("final_project/songs")
-    for i in song_list:
-        print(f"{index}: {i}")
-        index += 1
-    song = input(":") 
-    play_song(f"final_project/songs/{song_list[int(song) - 1]}")
+    #folder = list_folder("final_project/songs")
+    #song = input("Select a song to play: ")
+    #play_song(f"final_project/songs/{folder[int(song) - 1]}")
+
+    while True:
+        select = ""
+        if select == "1":
+            list_folder("final_project/songs")
+        if select == "2":
+            play_song()
 
 if __name__ == "__main__":
     main()
